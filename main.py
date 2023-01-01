c41017e3abdf5a1f6f6c1e43797e9022f0ce175c
Ampfar10 <94465412+Ampfar10@users.noreply.github.com>
2023-01-01 09:43:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,30 +4,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 def get_wifi_info():
-    # Run the 'netsh wlan show profiles' command to get a list of WiFi profiles
-    profiles = subprocess.run(['netsh', 'wlan', 'show', 'profiles'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    # Run the 'nmcli --terse --fields name,ssid,bssid,signal device wifi list' command to get a list of WiFi profiles
+    profiles = subprocess.run(['nmcli', '--terse', '--fields', 'name,ssid,bssid,signal', 'device', 'wifi', 'list'], stdout=subprocess.PIPE).stdout.decode('utf-8')
 
-    # Extract the names of the WiFi profiles
-    profiles = profiles.split('\n')
-    profiles = [p.strip() for p in profiles if 'Profile' in p]
-    profiles = [p.split(': ')[1] for p in profiles]
-
-    # Run the 'netsh wlan show profile name=PROFILE_NAME key=clear' command for each profile to get the password
+    # Extract the WiFi names and passwords from the command output
     wifi_info = []
-    for profile in profiles:
-        result = subprocess.run(['netsh', 'wlan', 'show', 'profile', f'name={profile}', 'key=clear'], stdout=subprocess.PIPE).stdout.decode('utf-8')
-        lines = result.split('\n')
-
-        # Extract the WiFi name and password from the command output
-        name = None
+    for profile in profiles.split('\n'):
+        fields = profile.split(':')
+        if len(fields) < 3:
+            continue
+        name = fields[1]
         password = None
-        for line in lines:
-            if 'SSID name' in line:
-                name = line.split(': ')[1]
-            elif 'Key Content' in line:
-                password = line.split(': ')[1]
-
-        # Add the WiFi name and password to the list
+        if fields[2] == '--':
+            password = fields[3]
         wifi_info.append({'name': name, 'password': password})
 
     return wifi_info
